423ab30973764235be0d67a721ac7d0cf88f5237
Anurag <59769356+Anuragreat@users.noreply.github.com>
2025-09-03 10:24:22 +0530

    Update wsgi.py

--- agri/wsgi.py
+++ agri/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
-
 from django.core.wsgi import get_wsgi_application
-
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agri.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
+
